ac3dd0e1eba884928ad785452c3b19543344d98e
BuildTools <unconfigured@null.spigotmc.org>
2024-09-23 19:16:12 -0700

    Added G constant

---
+++ src/consts.rs
@@ -0,0 +1,4 @@
+use crate::FLOAT;
+
+/// Gravitational constant in terms of light seconds^3 per jupiter mass per hour^2
+pub const GRAVITATIONAL_CONSTANT: FLOAT = 0.014195 * 6.67430e-11;
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
+pub mod consts;
 pub mod dynamic;
 
 type FLOAT = f32;
-
