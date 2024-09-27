7cf0090371176fe05a4e6379905100dcf541723b
BuildTools <unconfigured@null.spigotmc.org>
2024-09-26 17:03:12 -0700

    Added float constants in an extensible way

--- src/consts.rs
+++ src/consts.rs
@@ -2,3 +2,7 @@ use crate::Float;
 
 /// Gravitational constant in terms of light seconds^3 per jupiter mass per hour^2
 pub const GRAVITATIONAL_CONSTANT: Float = 0.0609_109;
+
+pub mod float {
+    pub use core::f32::consts::*;
+}
